d4201d9d3de29613e552da05e528c4f1feac203b
Mich V <michel@Michs-MacBook-Air.local>
2022-04-11 17:11:48 -0400

    Updated file paths

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + "/public"));
 
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/public/index.html");
 });
 app.get("/not-found", function (req, res) {
 
